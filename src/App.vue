d2702144bcb4457927ddbf8243e1d0074c439a5b
Arthur Enokyan <xazy06@gmail.com>
2019-01-09 20:25:07 +0300

    core

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,103 @@
 <template>
-  <div id="app">
+  <div class="m-grid m-grid--hor m-grid--root m-page" id="app">
     <v-header/>
 
-    <router-view/>
+    <!-- begin::Body -->
+    <div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">
+
+      <button class="m-aside-left-close m-aside-left-close--skin-dark" id="m_aside_left_close_btn"><i class="la la-close"></i></button>
+
+      <v-aside/>
+      <!-- BEGIN: Subheader -->
+      <div class="m-subheader ">
+        <div class="d-flex align-items-center">
+          <div class="mr-auto">
+            <h3 class="m-subheader__title m-subheader__title--separator">Dashboard 1</h3>
+            <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+              <li class="m-nav__item m-nav__item--home">
+                <a href="#" class="m-nav__link m-nav__link--icon">
+                  <i class="m-nav__link-icon la la-home"></i>
+                </a>
+              </li>
+              <li class="m-nav__separator">-</li>
+              <li class="m-nav__item">
+                <a href="" class="m-nav__link">
+                  <span class="m-nav__link-text">Actions</span>
+                </a>
+              </li>
+              <li class="m-nav__separator">-</li>
+              <li class="m-nav__item">
+                <a href="" class="m-nav__link">
+                  <span class="m-nav__link-text">Create New Post</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div class="m-dropdown m-dropdown--inline m-dropdown--arrow m-dropdown--align-right m-dropdown--align-push"
+                 m-dropdown-toggle="hover" aria-expanded="true">
+              <a href="#"
+                 class="m-portlet__nav-link btn btn-lg btn-secondary  m-btn m-btn--outline-2x m-btn--air m-btn--icon m-btn--icon-only m-btn--pill  m-dropdown__toggle">
+                <i class="la la-plus m--hide"></i>
+                <i class="la la-ellipsis-h"></i>
+              </a>
+
+              <div class="m-dropdown__wrapper">
+                <span class="m-dropdown__arrow m-dropdown__arrow--right m-dropdown__arrow--adjust"></span>
+                <div class="m-dropdown__inner">
+                  <div class="m-dropdown__body">
+                    <div class="m-dropdown__content">
+                      <ul class="m-nav">
+                        <li class="m-nav__section m-nav__section--first m--hide">
+                          <span class="m-nav__section-text">Quick Actions</span>
+                        </li>
+                        <li class="m-nav__item">
+                          <a href="" class="m-nav__link">
+                            <i class="m-nav__link-icon flaticon-share"></i>
+                            <span class="m-nav__link-text">Activity</span>
+                          </a>
+                        </li>
+                        <li class="m-nav__item">
+                          <a href="" class="m-nav__link">
+                            <i class="m-nav__link-icon flaticon-chat-1"></i>
+                            <span class="m-nav__link-text">Messages</span>
+                          </a>
+                        </li>
+                        <li class="m-nav__item">
+                          <a href="" class="m-nav__link">
+                            <i class="m-nav__link-icon flaticon-info"></i>
+                            <span class="m-nav__link-text">FAQ</span>
+                          </a>
+                        </li>
+                        <li class="m-nav__item">
+                          <a href="" class="m-nav__link">
+                            <i class="m-nav__link-icon flaticon-lifebuoy"></i>
+                            <span class="m-nav__link-text">Support</span>
+                          </a>
+                        </li>
+                        <li class="m-nav__separator m-nav__separator--fit">
+                        </li>
+                        <li class="m-nav__item">
+                          <a href="#" class="btn btn-outline-danger m-btn m-btn--pill m-btn--wide btn-sm">Submit</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- END: Subheader -->
+
+      <div class="m-content">
+        <router-view/>
+      </div>
+
+    </div>
+    <!-- end:: Body -->
 
     <v-footer/>
   </div>
@@ -11,23 +106,18 @@
 <script>
   import Header from '@/components/Header.vue'
   import Footer from '@/components/Footer.vue'
+  import AsideMenu from '@/components/AsideMenu.vue'
 
   export default {
     name: 'App',
     components: {
       'v-header': Header,
-      'v-footer': Footer
+      'v-footer': Footer,
+      'v-aside': AsideMenu
     },
   }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 10px;
-}
+<style lang="scss">
+  @import "/sass/style.scss";
 </style>
